ef41d349a8358d5f4acf5ab854323b4924db115a
David Pessanha <davidppessanha@gmail.com>
2021-09-03 20:41:35 -0300

    Criado o método quizrooms#show no controller para 2 situações: usuário professor e usuário estudante

--- app/controllers/quizrooms_controller.rb
+++ app/controllers/quizrooms_controller.rb
@@ -2,4 +2,25 @@ class QuizroomsController < ApplicationController
   def index
     @quizrooms = Quizroom.all
   end
+
+  def show
+    @quizroom = Quizroom.find(params[:id])
+    if current_user.teacher
+      @questions = set_questions
+    else
+      @questions = filter_done_questions(set_questions)
+    end
+  end
+
+  private
+
+  def set_questions
+    Question.where(subject: @quizroom.name)
+  end
+
+  def filter_done_questions(questions)
+    answers = Answer.where(user: current_user).includes(:question)
+    questions_done = answers.map { |answer| answer.question }
+    questions - questions_done
+  end
 end
