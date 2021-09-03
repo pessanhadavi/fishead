98f6fb98168a68adb44855e57ac03f6309f9e0f1
David Pessanha <davidppessanha@gmail.com>
2021-09-03 20:47:42 -0300

    Criada rota para questions#show aninhada à rota para quizrooms#show para que seja passada informação sobre o canal

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'quizrooms#index'
 
-  resources :quizrooms, only: [ :show ]
+  resources :quizrooms, only: [ :show ] do
+    resources :questions, only: [ :show ]
+  end
 end
