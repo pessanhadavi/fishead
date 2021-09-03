46c9788e3b1cd1a47b5898eb7efe7b992876f8c5
David Pessanha <davidppessanha@gmail.com>
2021-09-03 20:31:23 -0300

    Configurada a possibilidade de remember me para login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :questions
   has_many :answers
 
-  devise :database_authenticatable, :validatable
+  devise :database_authenticatable, :validatable, :rememberable
 end
