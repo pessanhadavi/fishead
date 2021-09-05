3c9cba4d78c429a471e677ac242c5bf8cb8b7917
David Pessanha <davidppessanha@gmail.com>
2021-09-05 17:14:25 -0300

    Arquivo de seeds atualizado

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,89 +6,126 @@ Quizroom.destroy_all
 User.destroy_all
 
 # Inicializando: Quizrooms e Users
-quiz_room_a = Quizroom.create! name:"Geografia geral"
-quiz_room_b = Quizroom.create! name:"Geofísica"
+geografia_geral = Quizroom.create! name:"Geografia geral"
+astronomia = Quizroom.create! name:"Astronomia"
+geofisica = Quizroom.create! name:"Geofísica"
 teacher = User.create! name:"Thiago", email:"thiago@professor.com", password:"senha123", teacher:true
 student_a = User.create! name:"Paulo", email:"paulo@estudante.com", password:"senha123"
 student_b = User.create! name:"Olívia", email:"olivia@estudante.com", password:"senha123"
 
 # Criando Questões: Question
-question_a = Question.new subject:"Geografia geral", utterance:"Qual é a capital do Brasil?"
-question_a.quizroom = quiz_room_a
-question_a.user = teacher
-question_a.save!
-
-question_b = Question.new subject:"Geofísica", utterance:"A Lua é um(a):"
-question_b.quizroom = quiz_room_b
-question_b.user = teacher
-question_b.save!
-
-question_c = Question.new subject:"Geografia geral", utterance:"Em que continente se localiza o Vietnã?"
-question_c.quizroom = quiz_room_a
-question_c.user = teacher
-question_c.save!
+def create_question(subject, utterance, quizroom, options, correct_option)
+  question = Question.new subject: subject, utterance: utterance
+  question.quizroom = quizroom
+  question.user = User.first
+  question.save!
+  create_choices(options, correct_option, question, question.quizroom)
+end
 
 # Criando opções: Choices
-options_a = ["Brasília", "Rio de Janeiro", "São Paulo", "Belo Horizonte", "Salvador"]
-options_a.each do |option|
-  choice = Choice.new
-  choice.option = option
-  choice.correct_option = option == "Brasília"
-  choice.question = question_a
-  choice.quizroom = quiz_room_a
-  choice.save!
+def create_choices(options, correct_option, question, quizroom)
+  options.each do |option|
+    choice = Choice.new
+    choice.option = option
+    choice.correct_option = option == correct_option
+    choice.question = question
+    choice.quizroom = quizroom
+    choice.save!
+  end
 end
 
-options_b = ["Estrela", "Planeta", "Satélite Natural", "Cometa", "Anã Branca"]
-options_b.each do |option|
-  choice = Choice.new
-  choice.option = option
-  choice.correct_option = option == "Satélite Natural"
-  choice.question = question_b
-  choice.quizroom = quiz_room_b
-  choice.save!
-end
+create_question(
+  "Geografia geral",
+  "Qual é a capital do Brasil?",
+  geografia_geral,
+  options = ["Brasília", "Rio de Janeiro", "São Paulo", "Belo Horizonte", "Salvador"],
+  options[0]
+)
 
-options_c = ["América", "Oceania", "África", "Europa", "Ásia"]
-options_c.each do |option|
-  choice = Choice.new
-  choice.option = option
-  choice.correct_option = option == "Ásia"
-  choice.question = question_c
-  choice.quizroom = quiz_room_a
-  choice.save!
-end
+create_question(
+  "Geografia geral",
+  "Em que continente se localiza o Vietnã?",
+  geografia_geral,
+  options = ["África", "América", "Oceania", "Ásia", "Europa"],
+  options[3]
+)
 
-# Criando respostas à uma questão: Answer
-# Para questão a
-find_choice_1a = Choice.find_by option: "Rio de Janeiro"
-answer_1a = Answer.new
-answer_1a.choice = find_choice_1a
-answer_1a.question = question_a
-answer_1a.quizroom = quiz_room_a
-answer_1a.user = student_a
-answer_1a.save!
+create_question(
+  "Geografia geral",
+  "Qual é o clima do Brasil?",
+  geografia_geral,
+  options = ["Temperado", "Polar", "Tropical", "Desértico", "Mediterrâneo"],
+  options[2]
+)
 
-find_choice_2a = Choice.find_by option: "Brasília"
-answer_2a = Answer.new
-answer_2a.choice = find_choice_2a
-answer_2a.question = question_a
-answer_2a.quizroom = quiz_room_a
-answer_2a.user = student_b
-answer_2a.save!
-# Para questão b
-find_choice_1b = Choice.find_by option: "Satélite Natural"
-answer_1b = Answer.new
-answer_1b.choice = find_choice_1b
-answer_1b.question = question_b
-answer_1b.quizroom = quiz_room_b
-answer_1b.user = student_a
-answer_1b.save!
+create_question(
+  "Geografia geral",
+  "Qual desses se encontra no Sul do Brasil?",
+  geografia_geral,
+  options = ["Ceará", "São Paulo", "Sergipe", "Paraná", "Roraima"],
+  options[3]
+)
 
-find_choice_2b = Choice.find_by option: "Anã Branca"
-answer_2b = Answer.new
-answer_2b.choice = find_choice_2b
-answer_2b.question = question_b
-answer_2b.quizroom = quiz_room_b
-answer_2b.user = student_b
-answer_2b.save!
+create_question(
+  "Astronomia",
+  "A lua é um(a):",
+  astronomia,
+  options = ["Planeta", "Satélite Natural", "Estrela", "Cometa", "Anã Branca"],
+  options[1]
+)
+
+create_question(
+  "Astronomia",
+  "O 2º planeta do Sistema Solar é...",
+  astronomia,
+  options = ["Marte", "Terra", "Saturno", "Plutão", "Vênus"],
+  options[4]
+)
+
+create_question(
+  "Astronomia",
+  "Quantos planetas tem o Sistema Solar?",
+  astronomia,
+  options = ["10", "9", "11", "8", "7"],
+  options[1]
+)
+
+create_question(
+  "Astronomia",
+  "De onde vem o superman?",
+  astronomia,
+  options = ["Piauí", "Marte", "Krypton", "EUA", "Andrômeda"],
+  options[1]
+)
+
+create_question(
+  "Geofísica",
+  "Qual deles é um gato da Ângela?",
+  geofisica,
+  options = ["Bandit", "Fluffy", "Bob", "Pepper", "Cookie"],
+  options[0]
+)
+
+create_question(
+  "Geofísica",
+  "Qual desses você escolhe?",
+  geofisica,
+  options = ["Karate", "Bears", "Beets", "Battlestar Galactica", "Todas as opções"],
+  options[4]
+)
+
+create_question(
+  "Geofísica",
+  "Andy é um bom personagem?",
+  geofisica,
+  options = ["Sim", "Sim", "Não", "Sim", "Sim"],
+  options[2]
+)
+
+create_question(
+  "Geofísica",
+  "Quem é o primeiro noivo de Pam?",
+  geofisica,
+  options = ["Jim", "Roy", "Dwight", "Michael", "Toby"],
+  options[1]
+)
