f67f3926739e9f93de80e84df9dfeadfc53aae2f
David Pessanha <davidppessanha@gmail.com>
2021-09-06 17:01:50 -0300

    testes automatizados para modelos: question

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Question, :type => :model do
+  fixtures :quizrooms, :users, :questions
+
+  before(:each) do
+    @question = questions(:dunder_question)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@question).to be_valid
+  end
+
+  it "is not valid without an utterance" do
+    @question.utterance = nil
+    expect(@question).to_not be_valid
+  end
+
+  it "is not valid without a subject" do
+    @question.subject = nil
+    expect(@question).to_not be_valid
+  end
+end
